3b882e690de7ff98393849ac6f4fbe83a87798a9
missiauthenticity100-ctrl <missiauthenticity100@gmail.com>
2026-04-28 03:22:03 -0500

    Create BTCBlack.sol

---
+++ contracts/BTCBlack.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.24;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract BTCBlack is ERC20 {
+    constructor() ERC20("Bitcoin Black", "BTC-B") {
+        _mint(msg.sender, 21_000_000 * 10**18);
+    }
+}
